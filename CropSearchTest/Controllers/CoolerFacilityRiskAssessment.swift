f04e833be49d344d089874619780a583338d9564
Valerie <glickvalerie@gmail.com>
2021-09-06 18:28:08 -0500

    Feat: [CST-003] Table View and Cell configuration

--- CropSearchTest/Controllers/CoolerFacilityRiskAssessment.swift
+++ CropSearchTest/Controllers/CoolerFacilityRiskAssessment.swift
@@ -10,8 +10,22 @@ import UIKit
 
 final class CoolerFacilityRiskAssessments: UITableViewController {
 
+    private var items: [RiskAssessmentTableViewCell.Model] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(RiskAssessmentTableViewCell.self, forCellReuseIdentifier: "RiskAssessmentTableViewCell")
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+
+        demo()
 
         view.backgroundColor = UIColor.systemBackground
     }
@@ -24,5 +38,47 @@ final class CoolerFacilityRiskAssessments: UITableViewController {
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
 
         navigationController?.toolbar.items = [flexibleSpace, leftButton, flexibleSpace, rightButton, flexibleSpace]
+    }
+}
+
+extension CoolerFacilityRiskAssessments {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RiskAssessmentTableViewCell", for: indexPath) as? RiskAssessmentTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: items[indexPath.row])
+        cell.delegate = self
+
+        return cell
+    }
+}
+
+extension CoolerFacilityRiskAssessments {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return items[indexPath.row].isExtended ? 100.0 : 50.0
+    }
+}
+
+extension CoolerFacilityRiskAssessments: RiskAssessmentTableViewCellDelegate {
+    func onCommentButtonPress(_ indexPath: IndexPath) {
+        items[indexPath.row].isExtended.toggle()
+        tableView.reloadData()
+    }
+}
+
+private extension CoolerFacilityRiskAssessments {
+    func demo() {
+        items = [
+            RiskAssessmentTableViewCell.Model(title: "Surrounding Areas / Adjacent Activities", isExtended: false, status: nil, indexPath: IndexPath(row: 0, section: 0)),
+            RiskAssessmentTableViewCell.Model(title: "Building Grounds", isExtended: false, status: nil, indexPath: IndexPath(row: 1, section: 0)),
+            RiskAssessmentTableViewCell.Model(title: "Building Structure", isExtended: true, status: .unacceptable, indexPath: IndexPath(row: 2, section: 0)),
+            RiskAssessmentTableViewCell.Model(title: "Water System", isExtended: false, status: nil, indexPath: IndexPath(row: 3, section: 0)),
+            RiskAssessmentTableViewCell.Model(title: "Other", isExtended: false, status: nil, indexPath: IndexPath(row: 4, section: 0))
+        ]
+
     }
 }
--- CropSearchTest/View/Cells/RiskAssessmentTableViewCell.swift
+++ CropSearchTest/View/Cells/RiskAssessmentTableViewCell.swift
@@ -8,12 +8,19 @@
 import Foundation
 import UIKit
 
+protocol RiskAssessmentTableViewCellDelegate: AnyObject {
+    func onCommentButtonPress(_ indexPath: IndexPath)
+}
+
 final class RiskAssessmentTableViewCell: UITableViewCell {
 
     private var titleLabel: UILabel = UILabel()
     private var commentButton: UIButton = UIButton()
     private var statusSegmentedControl: UISegmentedControl = UISegmentedControl(items: ["Acceptable", "Unacceptable", "N/A"])
     private var commentTextField: UITextField = UITextField()
+    private var indexPath: IndexPath = IndexPath(row: 0, section: 0)
+
+    weak var delegate: RiskAssessmentTableViewCellDelegate?
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -30,34 +37,41 @@ private extension RiskAssessmentTableViewCell {
     func setupView() {
         selectionStyle = .none
 
-        addSubview(titleLabel)
+        contentView.addSubview(titleLabel)
 
-        addSubview(commentButton)
+        commentButton.addTarget(self, action: #selector(onCommentButtonPress), for: .touchUpInside)
+        contentView.addSubview(commentButton)
 
         statusSegmentedControl.addTarget(self, action: #selector(onSegmentedControlChange), for: .valueChanged)
-        addSubview(statusSegmentedControl)
+        contentView.addSubview(statusSegmentedControl)
 
-        addSubview(commentTextField)
+        commentTextField.borderStyle = .roundedRect
+        contentView.addSubview(commentTextField)
 
         setupConstraints()
     }
 
     func setupConstraints() {
+        translatesAutoresizingMaskIntoConstraints = false
+
         titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16).isActive = true
         titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16).isActive = true
         titleLabel.trailingAnchor.constraint(greaterThanOrEqualTo: commentButton.leadingAnchor, constant: 8).isActive = true
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
 
         commentButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor).isActive = true
-        commentButton.trailingAnchor.constraint(equalTo: statusSegmentedControl.leadingAnchor, constant: 32).isActive = true
-        commentButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        commentButton.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        commentButton.trailingAnchor.constraint(equalTo: statusSegmentedControl.leadingAnchor, constant: -16).isActive = true
+        commentButton.translatesAutoresizingMaskIntoConstraints = false
 
-        statusSegmentedControl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 16).isActive = true
+        statusSegmentedControl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
         statusSegmentedControl.centerYAnchor.constraint(equalTo: commentButton.centerYAnchor).isActive = true
+        statusSegmentedControl.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3).isActive = true
+        statusSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
 
         commentTextField.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
-        commentButton.trailingAnchor.constraint(equalTo: statusSegmentedControl.trailingAnchor).isActive = true
+        commentTextField.trailingAnchor.constraint(equalTo: statusSegmentedControl.trailingAnchor).isActive = true
         commentTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        commentTextField.translatesAutoresizingMaskIntoConstraints = false
     }
 
     @objc func onSegmentedControlChange(_ segmentedControl: UISegmentedControl) {
@@ -71,23 +85,25 @@ private extension RiskAssessmentTableViewCell {
         default:
             break
         }
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+    }
+
+    @objc func onCommentButtonPress(_ button: UIButton) {
+        delegate?.onCommentButtonPress(indexPath)
     }
 }
 
 extension RiskAssessmentTableViewCell {
-    enum Appearance {
-        case basic, extended
-    }
-
-    enum Status {
+    enum Status: Int {
         case acceptable, unacceptable
         case notApplicable
     }
 
     struct Model {
         let title: String
-        let appearance: Appearance
-        let status: Status?
+        var isExtended: Bool
+        var status: Status?
+        let indexPath: IndexPath
     }
 }
 
@@ -95,13 +111,15 @@ extension RiskAssessmentTableViewCell {
     func configure(with model: Model) {
         titleLabel.text = model.title
 
-        switch model.appearance {
-        case .basic:
-            commentButton.setImage(UIImage(named: ""), for: .normal)
-            commentTextField.isHidden = true
-        case .extended:
-            commentButton.setImage(UIImage(named: ""), for: .normal)
-            commentTextField.isHidden = false
+        indexPath = model.indexPath
+        backgroundColor = indexPath.row % 2 == 1 ? .secondarySystemBackground : .systemBackground
+
+        commentButton.setImage(model.isExtended ? .remove : .add, for: .normal)
+        commentTextField.isHidden = !model.isExtended
+
+        if let status = model.status {
+            statusSegmentedControl.selectedSegmentIndex = status.rawValue
+            onSegmentedControlChange(statusSegmentedControl)
         }
     }
 }
